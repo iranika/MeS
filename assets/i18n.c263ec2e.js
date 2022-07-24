bd977f78f5e6ad6317791332aaf7f39ffb382a92
iranika <7370486+iranika@users.noreply.github.com>
2022-07-24 11:42:04 +0900

    Updates

--- assets/i18n.c263ec2e.js
+++ assets/i18n.c263ec2e.js
@@ -1,4 +1,4 @@
-import{h as $,r as G,c as B,w as Fe,F as xe,i as kt,g as $e,a as pt,o as wt,b as St,d as ve,T as ke,e as Ot}from"./index.8714ff11.js";/*!
+import{h as $,r as G,c as B,w as Fe,F as xe,i as kt,g as $e,a as pt,o as wt,b as St,d as ve,T as ke,e as Ot}from"./index.f93ddd5e.js";/*!
   * @intlify/shared v9.1.10
   * (c) 2022 kazuya kawaguchi
   * Released under the MIT License.
