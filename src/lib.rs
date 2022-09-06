34e78987912a2ccfb5557c377900dd1103cbcf81
iranika <7370486+iranika@users.noreply.github.com>
2022-09-06 19:17:31 +0900

    fix: コンフリクト解消の際にlib.rsがmes.rsで上書きされていたので修正

--- src/lib.rs
+++ src/lib.rs
@@ -1,199 +1,119 @@
-use std::default;
-
+use mes::Medo;
 use serde::{Deserialize, Serialize};
+use config::{Config};
 
-use super::{parse_mes, RawMedo, parseRawMedo, MedoPiece};
-use crate::mes::{Medo};
-use config::{Config, File};
-
-/* MeS Config関連のコード */
-#[derive(Debug, Deserialize, Serialize, Default)]
-pub struct MeSConfig{
-    pub name: String,
-    /// Default is "----\n"
-    pub header_delimiter: String,
-    pub flat_dialogue_config: FlatDialogueConfig,
-    pub medo_piece_config: MedoPieceConfig
+macro_rules! if_wasm {
+    ($($item:item)*) => {$(
+        #[cfg(target_arch = "wasm32")]
+        $item
+    )*}
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct MedoPieceConfig {
-    pub block_delimiter: String,
-    //以下、アトリビュートのメンバ
-    pub decorator: MedoPieceDecorator,
+macro_rules! if_hyper {
+    ($($item:item)*) => {$(
+        #[cfg(not(target_arch = "wasm32"))]
+        $item
+    )*}
 }
 
-impl Default for MedoPieceConfig{
-    fn default() -> Self {
-        Self { 
-            block_delimiter: "\n\n".to_string(),
-            decorator: Default::default()
-        }
+if_hyper! {
+    pub mod mes;
+
+    mod test_mes;
+
+    #[inline(always)]
+    pub fn parseMeSToJson(text: &str) -> String {
+        let conf = mes::builder::new();
+        let result = mes::parseMeSToJson(text, &conf);
+        result
     }
-}
-
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct MedoPieceDecorator {
-    pub dialogue: Vec<char>,
-    pub comments: Vec<char>,
-    pub sound_note: Vec<char>,
-    pub charactor: Vec<char>,
-    pub sound_position: Vec<char>,
-    pub timing: Vec<char>
-}
-
-impl Default for MedoPieceDecorator {
-    fn default() -> Self {
-        Self {
-            //以下、アトリビュートのメンバ
-            dialogue: vec![],
-            comments: vec!['#','＃'], 
-            sound_note: vec!['$','＄'],
-            charactor: vec!['@','＠'],
-            sound_position: vec!['!','！'],
-            timing: vec!['&','＆'],
-        }
+    pub fn parseMeS(text: &str) -> Medo{
+        let conf = mes::builder::new();
+        let result = mes::parse_mes(text, &conf);
+        result
     }
-}
+    #[inline(always)]
+    pub fn getVTT(mes_text: &str) -> String{
+        mes::getVTT(mes_text, &mes::builder::new())
+    }
+    
+    #[inline(always)]
+    pub fn countDialogueWordToJson(text: &str) -> String {
+        let conf = mes::builder::new();
+        mes::countDialogueWordToJson(text, &conf)
+    }
 
-#[derive(Debug, Deserialize, Serialize, Default)]
-pub struct FlatDialogueConfig{
-    pub start_str: String,
-    pub end_str: String,
+
+
     
 }
 
+if_wasm! {
+    extern crate wasm_bindgen;
+    use wasm_bindgen::prelude::*;
+    pub use crate::mes::*; 
+    pub mod mes;
 
-#[derive(Debug, Deserialize, Serialize, Default)]
-pub struct CountConfig{
-    pub ignore_char: Vec<String>
-}
-#[derive(Debug, Deserialize, Serialize, Default)]
-pub struct ChatConfig{
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    #[cfg(feature = "wee_alloc")]
+    #[global_allocator]
+    static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    
+    #[wasm_bindgen(getter_with_clone)]
+    pub struct WasmMedo {
+        medo: crate::mes::Medo
+    }
 
-}
+    #[wasm_bindgen]
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct WasmMeSBuilder {
+        builder: crate::mes::builder::MeSBuilder
+    }
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct MeSBuilder{
-    pub mes_config: MeSConfig,
-    pub count_config: CountConfig,
-    pub chat_config: ChatConfig
-}
 
-impl Default for MeSBuilder {
-    fn default() -> Self {
-        //デフォルト設定の定義
-        Self {
-            mes_config: MeSConfig {
-                header_delimiter: "----\n".to_string(),
-                flat_dialogue_config: FlatDialogueConfig {
-                    start_str: "「".to_string(),
-                    end_str: "」".to_string(),
-                    ..Default::default()
-                },
-                ..Default::default() //残りはDefaultのデフォルトをセットする
-            },
-            count_config: CountConfig {
-                ignore_char: vec![],
-                ..Default::default()
-            },
-            chat_config: ChatConfig {
-                ..Default::default()
 
-            }
+    #[wasm_bindgen]
+    impl WasmMeSBuilder {
+        #[wasm_bindgen]
+        pub fn getDefaultConfig() -> String {
+            serde_json::to_string(&mes::builder::new()).unwrap() 
+        }
+
+        pub fn showConfig(&self) -> String{
+            serde_json::to_string(self).unwrap()
         }
     }
-}
 
-impl MeSBuilder {
-    fn parseRawMedo(self: &Self, text: &str) -> RawMedo{
-        parseRawMedo(text, self)
+    
+    #[wasm_bindgen]
+    pub fn parseMeSToJson(text: &str) -> String {
+        mes::builder::new().parse_to_jsonstr(text)
     }
-}
-
-impl MeSBuilder {
-    pub fn parse(self: &Self, mes_text: &str) -> Medo{
-        let mut rawMedo = self.parseRawMedo(mes_text);
-        rawMedo.doflat(self);
-        return Medo{
-            header: rawMedo.parse_header().unwrap(),
-            body: rawMedo.parse_body(self).unwrap()
-        }
-        //parse_mes(mes_text)
-
-    }
-    pub fn parse_to_jsonstr(self: &Self, mes_text: &str) -> String{
-        let medo = self.parse(mes_text);
-        serde_json::to_string(&medo).unwrap()
-    }
-}
-
-pub fn new()->MeSBuilder{
-    let builder: MeSBuilder = Default::default();
-    builder
-}
-
-pub fn set_json_conf(json: &str)->MeSBuilder{
-    //TODO: JsonStringとのマージを実行できるようにする
-    let builder: MeSBuilder = serde_json::from_str(json).unwrap();
-    builder
-}
-
-
-#[cfg(test)]
-mod builder_test{
-
-    use crate::mes::{RawMedo, self};
-
-    use super::MeSBuilder;
-
-    //TODO: メジャーバージョンリリース時にテストデータを固定していく
-
-    #[test]
-    fn test_parseRawMedo(){
-        let text = std::fs::read_to_string("tests/SampleCommonScript.txt").unwrap();
-        let rawmedo: RawMedo = crate::mes::builder::new().parseRawMedo(&text);
-
-        println!("<header>{}</header>", rawmedo.header);
-        println!("<body>{}</body>", rawmedo.body);
-        
+    
+    #[wasm_bindgen]
+    pub fn parseMeSToJsonWithConf(text: &str, json: &str) -> String {
+        mes::builder::set_json_conf(json).parse_to_jsonstr(text)
     }
 
-    #[test]
-    fn test_parse(){
-        let text = std::fs::read_to_string("tests/SampleCommonScript.txt").unwrap();
-        let medo = crate::mes::builder::new().parse(&text);
-
-        println!("<header>{:?}</header>", medo.header);
-        println!("<body>{:?}</body>", medo.body);
-        
+    #[wasm_bindgen]
+    pub fn getDefaultConfigJson()->String{
+        serde_json::to_string(&mes::builder::new()).unwrap()
+    }
+    
+    #[wasm_bindgen]
+    pub fn countDialogueWordToJson(text: &str) -> String {
+        mes::countDialogueWordToJson(text, &mes::builder::new())
     }
 
-    #[test]
-    fn test_parse_to_jsonstr(){
-        let text = std::fs::read_to_string("tests/SampleCommonScript.txt").unwrap();
-        let json = crate::mes::builder::new().parse_to_jsonstr(&text);
-
-        println!("{}", json);
+    #[wasm_bindgen]
+    pub fn getVTT(text: &str) -> String {
+        mes::getVTT(text, &mes::builder::new())
     }
-
-    #[test]
-    fn test_parse_to_jsonstr_withconf(){
-        let text = std::fs::read_to_string("tests/SampleCommonScript.txt").unwrap();
-        let json = r#"
-{
-    "mes_config": {
-        "name": "",
-        "header_delimiter": "----\n",
-        "flat_dialogue_config": {
-        "start_str": "「",
-        "end_str": "」"
-        },
+    
+    #[wasm_bindgen]
+    pub fn echo(text: &str) -> String {
+        text.to_string()
     }
-}"#;
-        let djson = serde_json::to_string(&mes::builder::new()).unwrap();
-        let result = mes::builder::set_json_conf(&djson).parse_to_jsonstr(&text);
-        println!("{}", result);
-    }
-
+    
 }
